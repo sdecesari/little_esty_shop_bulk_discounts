da5251d857c2664af1ca77d7ff0bb4c38bd10f3c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-08-09 12:32:11 -0700

    Test: User story 7

--- spec/models/bulk_discounts_spec.rb
+++ spec/models/bulk_discounts_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe BulkDiscount, type: :model do
   
   describe 'relationships' do
     it { should belong_to(:merchant) }
+    
   end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe InvoiceItem, type: :model do
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_one(:merchant).through(:item) }
+    it { should have_many(:bulk_discounts).through(:merchant) }
+
   end
 
   describe "class methods" do
@@ -38,5 +41,40 @@ RSpec.describe InvoiceItem, type: :model do
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
     end
+  end
+
+  describe 'instance method' do 
+    before(:each) do
+      @m1 = Merchant.create!(name: 'Merchant 1')
+      @c1 = Customer.create!(first_name: 'Bilbo', last_name: 'Baggins')
+      @c2 = Customer.create!(first_name: 'Frodo', last_name: 'Baggins')
+      @c3 = Customer.create!(first_name: 'Samwise', last_name: 'Gamgee')
+      @c4 = Customer.create!(first_name: 'Aragorn', last_name: 'Elessar')
+      @c5 = Customer.create!(first_name: 'Arwen', last_name: 'Undomiel')
+      @c6 = Customer.create!(first_name: 'Legolas', last_name: 'Greenleaf')
+      @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
+      @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
+      @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)
+      @i1 = Invoice.create!(customer_id: @c1.id, status: 2)
+      @i2 = Invoice.create!(customer_id: @c1.id, status: 2)
+      @i3 = Invoice.create!(customer_id: @c2.id, status: 2)
+      @i4 = Invoice.create!(customer_id: @c3.id, status: 2)
+      @i5 = Invoice.create!(customer_id: @c4.id, status: 2)
+      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 0)
+      @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 4, unit_price: 8, status: 0)
+      @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
+      @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 2, unit_price: 5, status: 1)
+      @bd1 = BulkDiscount.create!(percent: 25, threshold: 5, merchant: @m1)
+      @bd2 = BulkDiscount.create!(percent: 20, threshold: 4, merchant: @m1)
+      @bd3 = BulkDiscount.create!(percent: 15, threshold: 3, merchant: @m1)
+      @bd4 = BulkDiscount.create!(percent: 10, threshold: 2, merchant: @m1)
+    end
+
+    it "discount_used" do 
+      expect(@ii_1.discount_used).to eq(@bd1)
+      expect(@ii_2.discount_used).to eq(@bd2)
+      expect(@ii_4.discount_used).to eq(@bd4)
+      expect(@ii_3.discount_used).to eq(nil)
+    end 
   end
 end
